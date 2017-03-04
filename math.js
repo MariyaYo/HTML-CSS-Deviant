96f3b8202a944793f71d3988b80c881920827a84
MariyaYo <black__sea@abv.bg>
2017-03-04 11:48:27 +0200

    First steps

---
+++ math.js
@@ -0,0 +1,24 @@
+function doAdd(){
+var Value1 = new Number(docoment.getElementById("Value1"), value);
+var Value2 = new Number(docoment.getElementById("Value2"), value);
+docoment.getElementByID("Result").setAttribute("value", Value1 + Value2);
+}
+
+function doSub(){
+	var Value1 = new Number(docoment.getElementById("Value1"), value);
+	var Value2 = new Number(docoment.getElementById("Value2"), value);
+	docoment.getElementByID("Result").setAttribute("value", Value1 - Value2);
+	}
+
+function doMul(){
+	var Value1 = new Number(docoment.getElementById("Value1"), value);
+	var Value2 = new Number(docoment.getElementById("Value2"), value);
+	docoment.getElementByID("Result").setAttribute("value", Value1 - Value2);
+	}
+
+function doDiv(){
+	var Value1 = new Number(docoment.getElementById("Value1"), value);
+	var Value2 = new Number(docoment.getElementById("Value2"), value);
+	docoment.getElementByID("Result").setAttribute("value", Value1 / Value2);
+	}
+
